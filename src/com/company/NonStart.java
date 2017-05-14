10a70d0dbff8684501ec42c450cd1920f47e4697
RiditaNizam <riditan@gmail.com>
2017-05-13 23:24:29 -0400

    Initial commit

---
+++ src/com/company/NonStart.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by ridita on 5/13/17.
+ */
+public class NonStart {
+    public String nonStart(String a, String b) {
+        return a.substring(1, a.length()) + b.substring(1, b.length());
+    }
+}
